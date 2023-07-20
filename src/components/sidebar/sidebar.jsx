fc422dac66cdf52504eead11727faf729753c37b
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-07-20 11:29:53 +0500

    Sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,9 +2,11 @@ import logo from '../../assets/logo.png'
 
 const Sidebar = () => {
     return (
-        <div className={`w-[100%] hidden md:flex bg-[#16c7ca] h-[100vh] sticky left-0`}>
+        <div className={`w-[20%] hidden md:flex bg-[#21576F] h-[100vh] fixed top-0 left-0`}>
             <div className={`w-[100%] mx-auto`}>
-                <img src={logo} className={`w-[80%] mx-auto mt-4`} alt="logo" />
+                {/* <img src={logo} className={`w-[80%] mx-auto mt-4`} alt="logo" /> */}
+                <p className={`text-[40px] text-center mt-8 text-white font-montserrat`}>SMART PRESS</p>
+                <div className={`h-[3px] bg-white w-[100%] mt-8`}></div>
             </div>
         </div>
     )
